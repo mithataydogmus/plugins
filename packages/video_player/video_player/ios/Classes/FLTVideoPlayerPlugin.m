9d6429b1a83b8651472e6d443f41df12718a274f
Mithat Aydoğmuş <mithataydogmus@gmail.com>
2021-01-03 17:04:10 +0300

    Change buffer seconds to 8

--- packages/video_player/video_player/ios/Classes/FLTVideoPlayerPlugin.m
+++ packages/video_player/video_player/ios/Classes/FLTVideoPlayerPlugin.m
@@ -229,7 +229,7 @@ static inline CGFloat radiansToDegrees(CGFloat radians) {
   };
 
   _player = [AVPlayer playerWithPlayerItem:item];
-  _player.currentItem.preferredForwardBufferDuration = 10;
+  _player.currentItem.preferredForwardBufferDuration = 8;
   _player.actionAtItemEnd = AVPlayerActionAtItemEndNone;
 
   [self createVideoOutputAndDisplayLink:frameUpdater];
