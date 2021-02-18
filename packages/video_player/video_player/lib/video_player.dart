9e813386639528ffdedd416002586d88d1566276
Mithat Aydoğmuş <mithataydogmus@gmail.com>
2021-02-18 12:09:55 +0300

    Disable video pause on lifeCycleState change

--- packages/video_player/video_player/lib/video_player.dart
+++ packages/video_player/video_player/lib/video_player.dart
@@ -572,7 +572,9 @@ class _VideoAppLifeCycleObserver extends Object with WidgetsBindingObserver {
     switch (state) {
       case AppLifecycleState.paused:
         _wasPlayingBeforePause = _controller.value.isPlaying;
-        _controller.pause();
+        if (!_controller.value.isShowingPIP) {
+          _controller.pause();
+        }
         break;
       case AppLifecycleState.resumed:
         if (_wasPlayingBeforePause) {
